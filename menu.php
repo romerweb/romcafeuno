c294bc8bbb7f8b0b801b60abe5d8a748ddcd3a77
Romer <gungonromer@gmail.com>
2024-09-03 09:31:30 +0300

    school project
    static cafe webpage

---
+++ menu.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 14px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+
+.lmargin-small-lh{
+	margin-left: 30px;
+	line-height: 5px;
+}
+.lmargin{
+	margin-left: 30px;
+}
+</style>
+
+
+<title>
+Login
+</title>
+<section class="lmargin-small-lh">
+<h2>Arellano University</h2>
+<h3>ETEEAP Online Class Attendance System (EOCAS)</h3>
+</section>
+</head>
+<body>
+
+
+</head>
+<body>
+
+<div class="topnav">
+  <a class="active" href="index.php">Log Out</a>
+  <a href="createclassmeeting.php">Create Class</a>
+    <a href="deleteclass.php">Delete Class</a>
+	  <a href="viewclasscode.php">View Online Classes</a>
+	    <a href="createattendance.php">Create Attendance</a>
+  <a href="viewattendance.php">View Attendance</a>
+    <a href="createuser.php">Add User</a>
+	  <a href="displaydatausers.php">View Users</a>
+</div>
+
+
+</body>
+</html>
